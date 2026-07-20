fef3d01a8a5ad133ea150c8e4936c9fc40fe59da
Tridintism23 <tridintstudio23@gmail.com>
2026-07-21 01:08:23 +0700

    thêm Models và Services để đọc ghi dữ liệu với file json

---
+++ Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusBookingSystem.Models
+{ 
+    public class Account
+    {
+        public string AccountId { get; set; } = Guid.NewGuid().ToString().Substring(0, 8);
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public UserRole Role { get; set; } = UserRole.Customer;
+    }
+}
