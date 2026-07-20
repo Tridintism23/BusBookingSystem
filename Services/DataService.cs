162461d7658a2f58a1e9f3018353988ed996dd1d
ktokto313 <kto3132006@gmail.com>
2026-07-21 02:47:30 +0700

    update

--- Services/DataService.cs
+++ Services/DataService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using BusBookingSystem.Models;
 using System.IO;
 using System.Text.Json;
+using Microsoft.Win32;
 
 namespace BusBookingSystem.Services
 {
@@ -19,23 +20,50 @@ namespace BusBookingSystem.Services
         public List<Bus> Buses { get; set; } = new();
         public List<Booking> Bookings { get; set; } = new();
 
-        public static List<T> LoadFromFile<T>(string fileName)
+        public static List<T> LoadFromFile<T>()
         {
-            string json = File.ReadAllText(fileName);
-            return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+                openFileDialog.Filter = "JSON (*.json)|*.json";
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    string json = File.ReadAllText(openFileDialog.FileName);
+                    return JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+                }
+            } catch (Exception _)
+            {
+                Console.WriteLine("Can't import this file into the system");
+            }
+            return [];
         }
 
         public static void SaveToFile<T>(string fileName, List<T> data)
         {
-            string json = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(fileName, json);
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+                saveFileDialog.Title = fileName;
+                saveFileDialog.DefaultExt = "json";
+                saveFileDialog.Filter = "JSON (*.json)|*.json";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    string json = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+                    File.WriteAllText(saveFileDialog.FileName, json);
+                }
+            } catch (Exception _)
+            {
+                Console.WriteLine("Error occurs when saving to file");
+            }
         }
 
         public void LoadAllData()
         {
-            Accounts = LoadFromFile<Account>(AccountsFile);
-            Buses = LoadFromFile<Bus>(BusesFile);
-            Bookings = LoadFromFile<Booking>(BookingsFile);
+            Accounts = LoadFromFile<Account>();
+            Buses = LoadFromFile<Bus>();
+            Bookings = LoadFromFile<Booking>();
         }
 
         public void SaveAccounts() => SaveToFile(AccountsFile, Accounts);
@@ -44,7 +72,7 @@ namespace BusBookingSystem.Services
     
         public DataService()
         {
-            LoadAllData();
+            //LoadAllData();
             if (Accounts.Count == 0)
             {
                 Accounts.Add(new Account { Username = "admin", Password = "123", FullName = "System Admin", Role = UserRole.Admin });
